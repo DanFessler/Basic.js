b8ba77bac7fb29d6afb5df27ceae16c00635e173
Dan Fessler <dan@manticoregames.com>
2018-09-05 17:16:12 -0700

    adding start script

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 // Read the file and print its contents.
-var fs = require("fs"),
-  filename = process.argv[2];
+let fs = require("fs");
+let filename = process.argv[2];
+
 fs.readFile(filename, "utf8", function(err, data) {
   if (err) throw err;
   BASIC(data);

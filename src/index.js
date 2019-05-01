9f34b245ae8007b33e9162292c5ee1bba21a54e2
DanFessler <fezzinate@gmail.com>
2019-05-01 14:30:59 -0700

    debug flag

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const Lexer = require("./lexer.js");
 const Parser = require("./parser.js");
 let basin = require("basin-script");
 
-module.exports = program => {
+module.exports = (program, debug) => {
   program = Lexer(program);
   program = Parser(program);
-  console.log(
-    "Program AST:",
-    "\n" + JSON.stringify(program, null, 1),
-    "\n\nResult:"
-  );
+  if (debug)
+    console.log(
+      "Program AST:",
+      "\n" + JSON.stringify(program, null, 1),
+      "\n\nResult:"
+    );
   basin.run(program);
 };

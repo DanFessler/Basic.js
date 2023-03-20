1619421a6ca6e4e00a11f07054b5a83a8aa74af2
fezz <fezzinate@gmail.com>
2023-03-20 03:30:42 -0700

    .

--- plugins/testPlugin.js
+++ plugins/testPlugin.js
@@ -1,3 +1,4 @@
 module.exports = {
-  shapoopie: test => console.log("The girl who's hard to get! ", test)
+  shapoopie: (test) => console.log("The girl who's hard to get! ", test),
+  rect: (x, y, w, h) => console.log("rect", { x, y, w, h }),
 };
